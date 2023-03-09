35b8b91dbe8f5bb88f92a83f3db624936bc29d10
Alvaro Farreny <farrenyalvaro@gmail.com>
2023-03-09 13:24:38 +0100

    new page Projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,19 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import portfolio from "../data/portfolio";
 import PortfolioItem from "./PortfolioItem";
 
 function Portfolio() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {portfolio.map((project) => (
-          <PortfolioItem
-            imgUrl={project.imgUrl}
-            title={project.title}
-            stack={project.stack}
-            link={project.link}
-          />
-        ))}
+    <div>
+      <div className="flex flex-col md:flex-row items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {portfolio.map((project) => (
+            <PortfolioItem
+              imgUrl={project.imgUrl}
+              title={project.title}
+              stack={project.stack}
+              link={project.link}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="text-center mt-5">
+        <Link
+          to="projects"
+          className="text-black dark:text-white px-3 py-1 border-2 border-stone-900 dark:border-white rounded-md "
+        >
+          All my projects
+        </Link>
       </div>
     </div>
   );
